809ce6c0cd2bf8209db978832d9eded2029e9397
김정수 <kjs20151240@gmail.com>
2024-08-20 16:26:44 +0900

    edit | page.tsx
    press start 버튼

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,40 @@
+'use client'
+
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter()
+
+  const goBattle = () => {
+    router.push('/BattleField')
+  }
+
   return (
-   <>
-    <h1>hello world</h1>
-   </>
+    <div className="flex items-center justify-center h-screen bg-gradient-to-b from-[rgb(var(--background-start-rgb))] to-[rgb(var(--background-end-rgb))]">
+    <button
+      className="
+        bg-blue-500
+        text-white
+        font-bold
+        py-4
+        px-12
+        rounded-lg
+        shadow-lg
+        transform
+        transition-transform
+        duration-300
+        hover:bg-blue-600
+        hover:scale-105
+        focus:outline-none
+        focus:ring-2
+        focus:ring-blue-300
+      "
+      onClick={goBattle}
+    >
+      Press Start!
+    </button>
+  </div>
+
   );
 }
